13770b18b960142211d1135fc3aa0949cc2cfd21
hazemBA20 <hazeminsat@gmail.com>
2025-02-24 20:09:25 +0100

    building the graph

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+const roads = [
+  "Alice's House-Bob's House",
+  "Alice's House-Cabin",
+  "Alice's House-Post Office",
+  "Bob's House-Town Hall",
+  "Daria's House-Ernie's House",
+  "Daria's House-Town Hall",
+  "Ernie's House-Grete's House",
+  "Grete's House-Farm",
+  "Grete's House-Shop",
+  "Marketplace-Farm",
+  "Marketplace-Post Office",
+  "Marketplace-Shop",
+  "Marketplace-Town Hall",
+  "Shop-Town Hall",
+];
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function findNodes([from, to], graph) {
+    if (from in graph) graph[from].push(to);
+    else graph[from] = [to];
+  }
+  for ([from, to] of edges.map((element) => element.split("-")))
+    findNodes([from, to], graph);
+  return graph;
+}
+console.log(buildGraph(roads));
